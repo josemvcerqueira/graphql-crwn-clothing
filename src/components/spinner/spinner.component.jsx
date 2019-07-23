26e84c5dc03f4fd5a5457914632bb2da9870ef28
Jose Cerqueira <jose@origensmedia.com>
2019-07-24 00:41:08 +0100

    Adding the apollo client to our application

--- src/components/spinner/spinner.component.jsx
+++ src/components/spinner/spinner.component.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import './spinner.styles.scss';
 
 const Spinner = () => (
-  <div className='spinner-overlay'>
-    <div className='spinner-container' />
+  <div className="spinner-overlay">
+    <div className="spinner-container" />
   </div>
 );
 
